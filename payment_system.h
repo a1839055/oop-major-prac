be7f96d2acc04f43f6bddb4aa9d49f1081a66784
Jonty Leslie <83710197+JLesDev@users.noreply.github.com>
2021-10-21 13:04:17 +1030

    testing yet again, may not work who knows

--- payment_system.h
+++ payment_system.h
@@ -1,12 +1,6 @@
-//
-//  payment_system.hpp
-//  
-//
-//  Created by Jonty Leslie on 5/10/21.
-//
 
-#ifndef payment_system_hpp
-#define payment_system_hpp
+#ifndef Payment_system_hpp
+#define Payment_system_hpp
 
 #include <stdio.h>
 #include <iostream>
@@ -16,67 +10,66 @@
 
 using namespace std;
 
-class payment{
-    private:
-    
-    public:
-        payment();
-        payment(float pPrice, string pCustomerName, int orderNum, int receiptNum, string order_name);
+class Payment{
+    protected:
         float price;
         string customer_name;
         string order_name;
         int order_id;
         int receipt_number;
-        bool is_payment_success;
-        void send_payment_info();
-        void send_order_to_kitchen();
-        virtual bool pay(); //pure virtual function
-        virtual string get_name();
-        ~payment(); //destructor
-};
-
-class cash : public payment{
-    private:
-        int cash_types[3];
+        bool is_Payment_success;
     public:
-        cash();
-        cash(float pBalance, payment pClassName);
-        float cash_till_balance;
-        float balance;
-        payment placeholder;
-        void cash_till();
-        bool pay();
-        ~cash();
+        Payment();
+        Payment(float p_price, string p_customer_name, int order_num, int receipt_num, string order_name);
+        ~Payment(); //destructor
+        
+
+        virtual bool pay() = 0; //pure virtual function
+        virtual string get_name();
 };
 
-class paypal : public payment{
+
+class Paypal : public Payment{
     private:
         string paypal_username;
         string paypal_password;
-    public:
-        paypal();
-        paypal(float pPayPalBalance, payment className);
         float paypal_balance;
-        payment placeholder;
+        float get_paypal_balance();
+    public:
+        Paypal();
+        Paypal(float p_price, string p_customer_name, int order_num, int receipt_num, string order_name);
+        // Payment placeholder;
         bool connect_to_paypal_servers();
         bool pay();
-        ~paypal();
+        ~Paypal();
 };
 
-class credit_card : public payment{
-    private:
-        int cc_number;
-        string cc_name;
-        int ccv;
-    public:
-        credit_card();
-        credit_card(float pBankBalance, payment className);
-        payment placeholder;
-        float bank_balance;
-        bool connect_credit_card_system();
-        bool pay();
-        ~credit_card();
+// class Cash : public Payment{
+//     public:
+//         Cash();
+//         Cash(float p_balance, Payment p_class_name);
+//         float cash_till_balance;
+//         float balance;
+//         // Payment placeholder;
+//         void cash_till();
+//         bool pay();
+//         ~Cash();
+// };
+
+// class CreditCard : public Payment{
+//     private:
+//         int cc_number;
+//         string cc_name;
+//         int ccv;
+//     public:
+//         CreditCard();
+//         CreditCard(float p_bank_balance, Payment class_name);
+//         // Payment placeholder;
+//         float bank_balance;
+//         bool connect_CreditCard_system();
+//         bool pay();
+//         ~CreditCard();
     
-};
+// };
 
-#endif /* payment_system_hpp */
+#endif /* Payment_system_hpp */
